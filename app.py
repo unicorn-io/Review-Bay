185e5d4cd800a460db92b76396c70b083807e01d
unicorn-io <tayalvishesh83@gmail.com>
2020-01-15 03:38:41 +0530

    Funtional Structure
    First Page struct

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/submit")
+def dash_page():
+    pass
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/fileUploaded", methods= ['POST'])
+def file_load():
+    try:
+        f = request.files['file']
+        f.save(f.filename)
+    except FileNotFoundError:
+        return redirect('/')
+    return render_template("fileu.html", name = f.filename)
+
+
+if __name__== "__main__":
+    app.run()
+
